fd69b9e0aa44a32cb95df18acf2f2229d0bfe35b
Ansara Hollinger <ansara.hooks@gmail.com>
2025-04-15 20:41:28 -0400

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import ListGroup from './components/ListGroup.tsx';
+import ProjectDisplay from './components/ProjectDisplay.tsx';
+import Dashboard from '../src/components/Dashboard.tsx';
 
 function App() {
-  const items = ['New York', 'Tokyo', 'London', 'Paris'];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading={'Cities'}
-        onSelectItem={handleSelectItem}
-      />
-    </div>
+    <>
+      <h1>Sewcial Media</h1>
+      <button>Add New Project!</button>
+      <ProjectDisplay />
+      <Dashboard />
+    </>
   );
 }
 
 export default App;
+
+// function App() {
+//   const items = ['New York', 'Tokyo', 'London', 'Paris'];
+
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading={'Cities'}
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/ProjectDisplay.tsx
@@ -0,0 +1,14 @@
+import ProjectCard from './ProjectCard.tsx';
+
+function ProjectDisplay() {
+  return (
+    <>
+      <div>
+        <h1>Project Card Display</h1>
+        <ProjectCard />
+      </div>
+    </>
+  );
+}
+
+export default ProjectDisplay;
